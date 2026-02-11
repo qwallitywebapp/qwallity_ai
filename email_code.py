9d29753f99207b71cdf76c4d2604541d48b8c18b
qwallitywebapp <118795299+qwallitywebapp@users.noreply.github.com>
2026-02-12 00:08:45 +0400

    Update email_code.py

--- email_code.py
+++ email_code.py
@@ -11,15 +11,11 @@
             "access": 0
         },
         {
-            "email": "aiqwallity24.09.2025@gmail.com",
-            "code": "aiqwallity2409",
+            "email": "qwallityaicourse15012026@gmail.com",
+            "code": "15012026@",
             "access": 0
         },
-        {
-            "email": "qwallitymanual03102025@gmail.com",
-            "code": "qwallity03102025",
-            "access": 0
-        }
+
         
     ]
 }
